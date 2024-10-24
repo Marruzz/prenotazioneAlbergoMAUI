a95fd0d55c45966ef613d0ef8aac8c88b47e2c2b
Marruzz <luca.marroni.07@outlook.com>
2024-10-24 21:25:23 +0200

    SISTEMATO IL CODICE

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,3 +1,6 @@
+using prenotazioneAlbergo;
+
+
 namespace prenotazioneAlbergo
 {
     public partial class MainPage : ContentPage
@@ -8,139 +11,45 @@ namespace prenotazioneAlbergo
             InitializeComponent();
         }
 
+        List<Prenotazione> prenotazioni = new List<Prenotazione>();
         private void BtnConferma_Clicked(object sender, EventArgs e)
         {
-            Prenotazione prenotazione = new Prenotazione();
+            try
+            {
+                Prenotazione p = new Prenotazione();
+                //serve UI
+                p.GiorniDiPrenotazione(dataInizio.Date, dataFine.Date);
+                p.NumeroPrenotati(pickerPrenotati.SelectedIndex + 1);
+                p.TipoDiCamera(pickerTipoStanza.SelectedIndex + 1);
+                p.SetParcheggio(CheckParcheggio.IsChecked);
+                List<string> nomi = new List<string>();
+                p.SetNomiPrenotati(nomi);
 
-            DateTime dataInizioInserita = dataInizio.Date;
-            DateTime dataFineInserita = dataFine.Date;
-
-
-
-            //giorni di prenotazione per sconto
-            TimeSpan giorniPrenotazione = dataFineInserita - dataInizioInserita;
-            int giorniPrenotati = (int)giorniPrenotazione.Days;
-            if (giorniPrenotati == 2)
-            {
-                prenotazione.sconto = 0;
+                //calcoli logici
+                p.Stagione(dataInizio.Date, dataFine.Date); //giorni di prenotazione
+                p.PrezzoGiornaliero(); //stagione e tipo di camera
+                p.ScontoDaApplicare(); //giorni di prenotazione
+                double prezzo = p.PrezzoTotale();
+                prenotazioni.Add(p);
+                DisplayAlert("Prenotazione Effettuata", "Il costo è di " + prezzo + " €", "OK, grazie");
             }
-            else if (giorniPrenotati > 2 && giorniPrenotati < 7)
+            catch (Exception ex)
             {
-                prenotazione.sconto = 25;
-            }
-            else
-            {
-                prenotazione.sconto = 35;
-            }
-
-            //giorni per stagionalità
-            if (dataInizioInserita.Month < 04)
-            {
-                prenotazione.Stagione = 1;
-            }
-            if (dataInizioInserita.Month > 04 && dataInizioInserita.Month < 09)
-            {
-                prenotazione.Stagione = 2;
-            }
-            else
-            {
-                prenotazione.Stagione = 3;
-            }
-
-            //picker numero di persone prenotate
-            if (pickerPrenotati.SelectedIndex != -1)
-            {
-                prenotazione.numeroPrenotati = pickerPrenotati.SelectedIndex + 1;
-            }
-            else
-            {
-                DisplayAlert("Errore", "Non hai selezionato il numero di persone prenotate", "OK");
-            }
-
-            //picker stanza selezionata
-            if (pickerTipoStanza.SelectedIndex != -1)
-            {
-                prenotazione.numeroPrenotati = pickerPrenotati.SelectedIndex + 1;
-            }
-            else
-            {
-                DisplayAlert("Errore", "Non hai selezionato che stanza si prenota", "OK");
-            }
-
-            //check del parcheggio
-            bool parcheggio = CheckParcheggio.IsChecked;
-            if (parcheggio)
-            {
-                prenotazione.parcheggio = true;
-            }
-
-            //prezzoAlGiorno
-            if (prenotazione.Stagione == 1)
-            {
-                if (prenotazione.tipoCamera == 1)
-                {
-                    prenotazione.prezzoAlGiorno = 15;
-                }
-                else if (prenotazione.tipoCamera == 2)
-                {
-                    prenotazione.prezzoAlGiorno = 30;
-                }
-                else
-                {
-                    prenotazione.prezzoAlGiorno = 45;
-                }
-            }
-            else if (prenotazione.Stagione == 2)
-            {
-                if (prenotazione.tipoCamera == 1)
-                {
-                    prenotazione.prezzoAlGiorno = 20;
-                }
-                else if (prenotazione.tipoCamera == 2)
-                {
-                    prenotazione.prezzoAlGiorno = 35;
-                }
-                else
-                {
-                    prenotazione.prezzoAlGiorno = 50;
-                }
-            }
-            else
-            {
-                if (prenotazione.tipoCamera == 1)
-                {
-                    prenotazione.prezzoAlGiorno = 25;
-                }
-                else if (prenotazione.tipoCamera == 2)
-                {
-                    prenotazione.prezzoAlGiorno = 40;
-                }
-                else
-                {
-                    prenotazione.prezzoAlGiorno = 55;
-                }
-            }
-
-            // calcolo del prezzo
-            double prezzoParziale = prenotazione.prezzoAlGiorno * prenotazione.numeroPrenotati;
-            if (prenotazione.sconto == 0)
-            {
-                prezzo = prenotazione.prezzoAlGiorno * giorniPrenotati;
-                DisplayAlert("Finito", "Il prezzo è di: " + prezzo + " euro", "OK");
-            }
-            else if (prenotazione.sconto == 25)
-            {
-                double sconto = 0.25;
-                prezzo = (prenotazione.prezzoAlGiorno * giorniPrenotati) - (prenotazione.prezzoAlGiorno * giorniPrenotati * 0.25);
-                DisplayAlert("Finito", "Il prezzo è di: " + prezzo + " euro", "OK");
-            }
-            else
-            {
-                double sconto = 0.35;
-                prezzo = (prenotazione.prezzoAlGiorno * giorniPrenotati) - (prenotazione.prezzoAlGiorno * giorniPrenotati * 0.25);
-                DisplayAlert("Finito", "Il prezzo è di: " + prezzo + " euro", "OK");
+                DisplayAlert("Errore", ex.Message, "OK");
             }
         }
-    }
 
+        private void PickerPrenotati_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Pulisci gli Entry esistenti
+            stackLayoutNomiPrenotati.Children.Clear();
+
+            // Aggiungi nuovi Entry in base alla selezione
+            for (int i = 0; i < pickerPrenotati.SelectedIndex + 1; i++)
+            {
+                stackLayoutNomiPrenotati.Children.Add(new Entry { Placeholder = $"Nome prenotato {i + 1}"});
+            }
+        }
+        
+    }
 }
--- Prenotazione.cs
+++ Prenotazione.cs
@@ -8,13 +8,159 @@ namespace prenotazioneAlbergo
 {
     internal class Prenotazione
     {
-        public string dataInizio { get; set; }
-        public string dataFine { get; set; }
-        public int prezzoAlGiorno { get; set; }
-        public int Stagione { get; set; }
-        public int tipoCamera { get; set; }
-        public int numeroPrenotati { get; set; }
-        public int sconto { get; set; }
-        public bool parcheggio { get; set; }
+        private string dataInizio { get; set; }
+        private string dataFine { get; set; }
+        private int prezzoAlGiorno { get; set; }
+        private int giorniPrenotati { get; set; }
+        private string stagione { get; set; }
+        private char tipoCamera { get; set; }
+        private int numeroPrenotati { get; set; }
+        private double sconto { get; set; }
+        private bool parcheggio { get; set; }
+        private List<string> nomiPrenotati;
+
+
+        //serve UI
+        public void GiorniDiPrenotazione(DateTime dataFineInserita, DateTime dataInizioInserita)
+        {
+            TimeSpan giorniPrenotazione = dataInizioInserita - dataFineInserita;
+            giorniPrenotati = (int)giorniPrenotazione.Days;
+
+        }
+        public void NumeroPrenotati(int prenotati)
+        {
+            numeroPrenotati = prenotati;
+        }
+        public void TipoDiCamera(int index)
+        {
+            if (index == 1)
+            {
+                tipoCamera = 'B';
+            }
+            else if (index == 2)
+            {
+                tipoCamera = 'M';
+            }
+            else
+            {
+                tipoCamera = 'A';
+            }
+        }
+        public void SetParcheggio(bool park)
+        {
+            parcheggio = park;
+        }
+        public void SetNomiPrenotati(List<string> nomiPrenotati)
+        {
+            List<string> newNomi = new List<string>();
+            foreach (string nome in nomiPrenotati)
+            {
+                newNomi.Add(nome);
+            }
+            nomiPrenotati.AddRange(newNomi);
+        }
+
+
+        //calcoli logici
+        public void ScontoDaApplicare()
+        {
+            if (giorniPrenotati <= 2)
+            {
+                sconto = 0;
+            }
+            else if (giorniPrenotati > 2 && giorniPrenotati < 7)
+            {
+                sconto = 0.25;
+            }
+            else
+            {
+                sconto = 0.35;
+            }
+        }
+        public void Stagione(DateTime dataFineInserita, DateTime dataInizioInserita)
+        {
+            if (dataInizioInserita.Month < 04)
+            {
+                stagione = "BS";
+            }
+            if (dataInizioInserita.Month > 04 && dataInizioInserita.Month < 09)
+            {
+                stagione = "AS";
+            }
+            else
+            {
+                stagione = "MS";
+            }
+        }
+        public void PrezzoGiornaliero()
+        {
+            if (stagione == "BS")
+            {
+                if (tipoCamera == 'B')
+                {
+                    prezzoAlGiorno = 15;
+                }
+                else if (tipoCamera == 'M')
+                {
+                    prezzoAlGiorno = 30;
+                }
+                else if (tipoCamera == 'A')
+                {
+                    prezzoAlGiorno = 45;
+                }
+            }
+            else if (stagione == "MS")
+            {
+                if (tipoCamera == 'B')
+                {
+                    prezzoAlGiorno = 20;
+                }
+                else if (tipoCamera == 'M')
+                {
+                    prezzoAlGiorno = 35;
+                }
+                else if (tipoCamera == 'A')
+                {
+                    prezzoAlGiorno = 50;
+                }
+            }
+            else
+            {
+                if (tipoCamera == 'B')
+                {
+                    prezzoAlGiorno = 25;
+                }
+                else if (tipoCamera == 'M')
+                {
+                    prezzoAlGiorno = 40;
+                }
+                else if (tipoCamera == 'A')
+                {
+                    prezzoAlGiorno = 55;
+                }
+            }
+        }
+        public double PrezzoTotale()
+        {
+            double prezzoParziale, scontoParziale, prezzoTotale, costoParcheggio = 0;
+
+            if (parcheggio)
+            {
+                costoParcheggio = 5 * giorniPrenotati;
+            }
+
+            if (sconto == 0)
+            {
+                prezzoTotale = prezzoAlGiorno * giorniPrenotati * numeroPrenotati + costoParcheggio;
+            }
+            else
+            {
+                scontoParziale = prezzoAlGiorno * giorniPrenotati * sconto * numeroPrenotati;
+                prezzoParziale = prezzoAlGiorno * giorniPrenotati * numeroPrenotati;
+                prezzoTotale = prezzoParziale - scontoParziale + costoParcheggio;
+            }
+            return prezzoTotale;
+        }
     }
+
 }
